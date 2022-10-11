791656cd1026ddf1fe5672d5b740ef1e9f1817af
Harsh-Jaiswal-CSE <77989064+Harsh-Jaiswal-CSE@users.noreply.github.com>
2022-10-11 21:37:54 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -2,8 +2,6 @@
  navbar=document.querySelector('.navbar')
  navList=document.querySelector('.nav-list')
  rightnav=document.querySelector('.rightnav')
- burger.addEventListener('click',()=>{
-	rightnav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
  	navbar.classList.toggle('h-nav-resp');
 
